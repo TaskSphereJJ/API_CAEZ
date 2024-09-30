37252b30022a586615c6163160812eed8d050645
Rodrigo Reyes <rodrigoreyes2902@gmail.com>
2024-09-29 23:10:28 -0600

    modulo turno :tada::sparkles:

--- src/turno/turno.module.ts
+++ src/turno/turno.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TurnoService } from './turno.service';
 import { TurnoController } from './turno.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Turno } from './entities/turno.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Turno])],
   controllers: [TurnoController],
   providers: [TurnoService],
 })
