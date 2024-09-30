183f4bedb676cdfeeac08d1ac3bfca0639bfbf81
Rodrigo Reyes <rodrigoreyes2902@gmail.com>
2024-09-29 23:08:44 -0600

    entidad usuario :tada::sparkles:

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,43 @@
-export class User {}
+import { Role } from "src/roles/entities/role.entity";
+import { BeforeInsert, Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+// Necesario para encriptar la contraseña
+import * as bcrypt from 'bcrypt';
+
+@Entity('users')
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    name: string;
+
+    @Column()
+    lastName: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    password: string;
+
+    @Column()
+    registrationDate: Date;
+
+    @ManyToOne(() => Role)
+    roleId: Role;
+
+    @Column({ default: true })
+    isActive: boolean;
+
+
+    //TypeORM se ejecuta antes de insertar un nuevo usuario en la base de datos
+    @BeforeInsert()
+    async hashPassword() {
+        this.password = await bcrypt.hash(this.password, 10);
+    }
+
+    // Método para verificar la contraseña
+    async checkPassword(password: string): Promise<boolean> {
+        return bcrypt.compare(password, this.password);
+    }
+}
