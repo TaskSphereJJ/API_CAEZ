d00c2c709e2e29f0f1d7232bf5a9bb73bd3b1dac
Rodrigo Reyes <rodrigoreyes2902@gmail.com>
2024-09-29 23:09:16 -0600

    create user :tada::sparkles:

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,29 @@
-export class CreateUserDto {}
+import { IsDate, IsNotEmpty, IsNumber, IsString } from "class-validator";
+
+export class CreateUserDto {
+
+    @IsNotEmpty()
+    @IsString()
+    name: string;
+
+    @IsNotEmpty()
+    @IsString()
+    lastName: string;
+
+    @IsNotEmpty()
+    @IsString()
+    email: string;
+
+    @IsNotEmpty()
+    @IsString()
+    password: string;
+
+    @IsNotEmpty()
+    @IsDate()
+    registrationDate: Date;
+
+    @IsNotEmpty()
+    @IsNumber()
+    roleId: number;
+
+}
