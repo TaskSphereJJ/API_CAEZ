b4a460392678de9dd21d490113ddca8863d4b238
Rodrigo Reyes <rodrigoreyes2902@gmail.com>
2024-09-30 23:04:55 -0600

    app module :tada::sparkles:

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,20 @@ import { UsersModule } from './users/users.module';
 import { DireccionModule } from './direccion/direccion.module';
 import { TurnoModule } from './turno/turno.module';
 import { GradosModule } from './grados/grados.module';
+import { SexoModule } from './sexo/sexo.module';
+import { ParentezcoModule } from './parentezco/parentezco.module';
+import { EnfermedadModule } from './enfermedad/enfermedad.module';
+import { MesModule } from './mes/mes.module';
+import { PadrinoModule } from './padrino/padrino.module';
+import { EncargadoModule } from './encargado/encargado.module';
+import { AlumnoModule } from './alumno/alumno.module';
+import { AlumnoGradoModule } from './alumno-grado/alumno-grado.module';
+import { PagoModule } from './pago/pago.module';
+import { PagoMesModule } from './pago-mes/pago-mes.module';
+import { FacturaModule } from './factura/factura.module';
+import { InformacionFinancieraModule } from './informacion-financiera/informacion-financiera.module';
+import { TipoDocModule } from './tipo-doc/tipo-doc.module';
+import { TipoPagoModule } from './tipo-pago/tipo-pago.module';
 
 @Module({
   imports: [
@@ -28,6 +42,20 @@ import { GradosModule } from './grados/grados.module';
     DireccionModule,
     TurnoModule,
     GradosModule,
+    SexoModule,
+    ParentezcoModule,
+    EnfermedadModule,
+    MesModule,
+    PadrinoModule,
+    EncargadoModule,
+    AlumnoModule,
+    AlumnoGradoModule,
+    PagoModule,
+    PagoMesModule,
+    FacturaModule,
+    InformacionFinancieraModule,
+    TipoDocModule,
+    TipoPagoModule,
   ],
   controllers: [AppController],
   providers: [AppService],
