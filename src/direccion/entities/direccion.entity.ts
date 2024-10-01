60d0b776118e959c0cc7f363313b69977ea75aeb
Rodrigo Reyes <rodrigoreyes2902@gmail.com>
2024-09-30 23:06:50 -0600

    entidad direccion :tada::sparkles:

--- src/direccion/entities/direccion.entity.ts
+++ src/direccion/entities/direccion.entity.ts
@@ -1,1 +1,13 @@
-export class Direccion {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('direccion')
+export class Direccion {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    name: string;
+
+    @Column({ default: true})
+    isActive: boolean;
+}
