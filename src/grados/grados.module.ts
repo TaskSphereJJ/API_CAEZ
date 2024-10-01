d4f83c53a25f4a1c6de4ddc40ed104b383bb8d3e
Rodrigo Reyes <rodrigoreyes2902@gmail.com>
2024-09-30 23:10:28 -0600

    modulo de grado :tada::sparkles:

--- src/grados/grados.module.ts
+++ src/grados/grados.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { GradosService } from './grados.service';
 import { GradosController } from './grados.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Grado } from './entities/grado.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Grado])],
   controllers: [GradosController],
   providers: [GradosService],
 })
