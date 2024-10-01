507ab833d608803c3b2205425e3a196fcdd83415
Rodrigo Reyes <rodrigoreyes2902@gmail.com>
2024-09-30 23:05:40 -0600

    modulo de direccion :tada::sparkles:

--- src/direccion/direccion.module.ts
+++ src/direccion/direccion.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DireccionService } from './direccion.service';
 import { DireccionController } from './direccion.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Direccion } from './entities/direccion.entity';
 
 @Module({
+  imports:[TypeOrmModule.forFeature([Direccion])],
   controllers: [DireccionController],
   providers: [DireccionService],
 })
