6ee933e631c81b797ed1ec2987e38ab6ac11264f
Rodrigo Reyes <rodrigoreyes2902@gmail.com>
2024-09-30 23:08:08 -0600

    modulo encargado :tada::sparkles:

--- src/encargado/encargado.module.ts
+++ src/encargado/encargado.module.ts
@@ -3,9 +3,13 @@ import { EncargadoService } from './encargado.service';
 import { EncargadoController } from './encargado.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Encargado } from './entities/encargado.entity';
+import { Role } from 'src/roles/entities/role.entity';
+import { Direccion } from 'src/direccion/entities/direccion.entity';
+import { Sexo } from 'src/sexo/entities/sexo.entity';
+import { User } from 'src/users/entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Encargado])],
+  imports: [TypeOrmModule.forFeature([Encargado, Role, Direccion, Sexo, User])],
   controllers: [EncargadoController],
   providers: [EncargadoService],
 })
