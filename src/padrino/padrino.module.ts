b81169e1003e2854ccc9372d6a609311576ea740
Rodrigo Reyes <rodrigoreyes2902@gmail.com>
2024-10-01 20:44:41 -0600

    padrino :tada::sparkles:

---
+++ src/padrino/padrino.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { PadrinoService } from './padrino.service';
+import { PadrinoController } from './padrino.controller';
+
+@Module({
+  controllers: [PadrinoController],
+  providers: [PadrinoService],
+})
+export class PadrinoModule {}
