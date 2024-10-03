6fa76eb61a06a9702b64cd6bb247c21645fe65b9
Rodrigo Reyes <rodrigoreyes2902@gmail.com>
2024-10-02 22:05:44 -0600

    servicio de alumno grado :tada::sparkles:

--- src/alumno-grado/alumno-grado.service.ts
+++ src/alumno-grado/alumno-grado.service.ts
@@ -4,22 +4,30 @@ import { UpdateAlumnoGradoDto } from './dto/update-alumno-grado.dto';
 
 @Injectable()
 export class AlumnoGradoService {
+
+
+
+  // Metodo crear
   create(createAlumnoGradoDto: CreateAlumnoGradoDto) {
     return 'This action adds a new alumnoGrado';
   }
 
+  // Metodo buscar todos
   findAll() {
     return `This action returns all alumnoGrado`;
   }
 
+  // Metodo buscar uno
   findOne(id: number) {
     return `This action returns a #${id} alumnoGrado`;
   }
 
+  // Metodo modificar
   update(id: number, updateAlumnoGradoDto: UpdateAlumnoGradoDto) {
     return `This action updates a #${id} alumnoGrado`;
   }
 
+  // Metodo eliminar
   remove(id: number) {
     return `This action removes a #${id} alumnoGrado`;
   }
