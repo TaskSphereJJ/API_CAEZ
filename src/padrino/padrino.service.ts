27aae620b19ad7859c89ae8497b9a8aa967bb301
Rodrigo Reyes <rodrigoreyes2902@gmail.com>
2024-10-02 22:15:11 -0600

    servicio padrino :tada::sparkles:

--- src/padrino/padrino.service.ts
+++ src/padrino/padrino.service.ts
@@ -4,22 +4,29 @@ import { UpdatePadrinoDto } from './dto/update-padrino.dto';
 
 @Injectable()
 export class PadrinoService {
+
+
+  // Crear padrino
   create(createPadrinoDto: CreatePadrinoDto) {
     return 'This action adds a new padrino';
   }
 
+  // Obtener todos los padrinos
   findAll() {
     return `This action returns all padrino`;
   }
 
+  // Obtenr un padrino por su id
   findOne(id: number) {
     return `This action returns a #${id} padrino`;
   }
 
+  // Modificar un padrino
   update(id: number, updatePadrinoDto: UpdatePadrinoDto) {
     return `This action updates a #${id} padrino`;
   }
 
+  // Eliminar un padrino
   remove(id: number) {
     return `This action removes a #${id} padrino`;
   }
