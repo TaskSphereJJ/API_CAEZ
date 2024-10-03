fa7fc388a85c1638f39ee99aa0ee966374b76d70
Rodrigo Reyes <rodrigoreyes2902@gmail.com>
2024-10-02 22:10:17 -0600

    servicio info-financiera :tada::sparkles:

--- src/informacion-financiera/informacion-financiera.service.ts
+++ src/informacion-financiera/informacion-financiera.service.ts
@@ -4,22 +4,29 @@ import { UpdateInformacionFinancieraDto } from './dto/update-informacion-financi
 
 @Injectable()
 export class InformacionFinancieraService {
+
+
+  // Crear info-financiera
   create(createInformacionFinancieraDto: CreateInformacionFinancieraDto) {
     return 'This action adds a new informacionFinanciera';
   }
 
+  // Obtener toda la info-financiera
   findAll() {
     return `This action returns all informacionFinanciera`;
   }
 
+  // Obtener una info por su id
   findOne(id: number) {
     return `This action returns a #${id} informacionFinanciera`;
   }
 
+  // Modificar info-financiera por su id
   update(id: number, updateInformacionFinancieraDto: UpdateInformacionFinancieraDto) {
     return `This action updates a #${id} informacionFinanciera`;
   }
 
+  // Eliminar una info
   remove(id: number) {
     return `This action removes a #${id} informacionFinanciera`;
   }
