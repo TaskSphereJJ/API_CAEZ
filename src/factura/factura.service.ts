02294439ec0e4b8e849b7edae9c9cd690e50befc
Rodrigo Reyes <rodrigoreyes2902@gmail.com>
2024-10-02 22:09:35 -0600

    servicio factura :tada::sparkles:

--- src/factura/factura.service.ts
+++ src/factura/factura.service.ts
@@ -4,22 +4,29 @@ import { UpdateFacturaDto } from './dto/update-factura.dto';
 
 @Injectable()
 export class FacturaService {
+
+
+  // Crear factura
   create(createFacturaDto: CreateFacturaDto) {
     return 'This action adds a new factura';
   }
 
+  // Obtener todas las facturas
   findAll() {
     return `This action returns all factura`;
   }
 
+  // Buscar una factura por su id
   findOne(id: number) {
     return `This action returns a #${id} factura`;
   }
 
+  // Modificar factura
   update(id: number, updateFacturaDto: UpdateFacturaDto) {
     return `This action updates a #${id} factura`;
   }
 
+  // Eliminar factura
   remove(id: number) {
     return `This action removes a #${id} factura`;
   }
