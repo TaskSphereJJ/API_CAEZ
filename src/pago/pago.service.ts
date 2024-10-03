a0a38d21f6040a807bed965f86450a23e797db13
Rodrigo Reyes <rodrigoreyes2902@gmail.com>
2024-10-02 22:15:34 -0600

    servicio pago :tada::sparkles:

--- src/pago/pago.service.ts
+++ src/pago/pago.service.ts
@@ -4,22 +4,30 @@ import { UpdatePagoDto } from './dto/update-pago.dto';
 
 @Injectable()
 export class PagoService {
+
+
+
+  // Crear pago
   create(createPagoDto: CreatePagoDto) {
     return 'This action adds a new pago';
   }
 
+  // Obtener todos los pagos
   findAll() {
     return `This action returns all pago`;
   }
 
+  // Buscar un pago por su id
   findOne(id: number) {
     return `This action returns a #${id} pago`;
   }
 
+  // Modificar un pago
   update(id: number, updatePagoDto: UpdatePagoDto) {
     return `This action updates a #${id} pago`;
   }
 
+  // Elimnar un pago
   remove(id: number) {
     return `This action removes a #${id} pago`;
   }
