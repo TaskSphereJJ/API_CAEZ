586f8eae96b4a6c5b8b25f920025727a911528eb
Rodrigo Reyes <rodrigoreyes2902@gmail.com>
2024-10-13 23:46:26 -0600

    servicio factura :tada::sparkles:

--- src/factura/factura.service.ts
+++ src/factura/factura.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { CreateFacturaDto } from './dto/create-factura.dto';
 import { UpdateFacturaDto } from './dto/update-factura.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Factura } from './entities/factura.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class FacturaService {
-  constructor() { }
+  constructor(
+    @InjectRepository(Factura) private readonly facturaRepository: Repository<Factura>,
+  ) { }
 
   // Crear factura
   create(createFacturaDto: CreateFacturaDto) {
