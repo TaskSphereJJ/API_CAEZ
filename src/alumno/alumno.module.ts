872ce07ade1f7ea1194f53b14313ef52fb718e99
Rodrigo Reyes <rodrigoreyes2902@gmail.com>
2024-10-21 11:02:52 -0600

    modulo alumno :tada::sparkles:

--- src/alumno/alumno.module.ts
+++ src/alumno/alumno.module.ts
@@ -3,9 +3,16 @@ import { AlumnoService } from './alumno.service';
 import { AlumnoController } from './alumno.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Alumno } from './entities/alumno.entity';
+import { Sexo } from 'src/sexo/entities/sexo.entity';
+import { Role } from 'src/roles/entities/role.entity';
+import { Encargado } from 'src/encargado/entities/encargado.entity';
+import { Enfermedad } from 'src/enfermedad/entities/enfermedad.entity';
+import { Turno } from 'src/turno/entities/turno.entity';
+import { Admin } from 'typeorm';
+import { Padrino } from 'src/padrino/entities/padrino.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Alumno])],
+  imports: [TypeOrmModule.forFeature([Alumno, Sexo, Role, Encargado, Enfermedad, Turno, Admin, Padrino])],
   controllers: [AlumnoController],
   providers: [AlumnoService],
 })
