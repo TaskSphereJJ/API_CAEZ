ef4f541c0d2ec44cf1a6d3165eacd2e06d0c5d95
Rodrigo Reyes <rodrigoreyes2902@gmail.com>
2024-10-21 11:05:01 -0600

    Dto alumno :tada::sparkles:

--- src/alumno/dto/update-alumno.dto.ts
+++ src/alumno/dto/update-alumno.dto.ts
@@ -1,4 +1,51 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateAlumnoDto } from './create-alumno.dto';
+import { IsBoolean, IsDate, IsNotEmpty, IsNumber, IsString } from "class-validator";
 
-export class UpdateAlumnoDto extends PartialType(CreateAlumnoDto) {}
+export class UpdateAlumnoDto {
+    @IsNotEmpty()
+    @IsString()
+    name: string;
+
+    @IsNotEmpty()
+    @IsString()
+    lastName: string;
+
+    @IsNotEmpty()
+    @IsDate()
+    registrationDate: Date;
+
+    @IsNotEmpty()
+    @IsNumber()
+    documentNumber: number; 
+
+    @IsNotEmpty()
+    @IsBoolean()
+    esBecado: boolean;
+
+    @IsNotEmpty()
+    @IsNumber()
+    sexoId: number;
+
+    @IsNotEmpty()
+    @IsNumber()
+    roleId: number;
+
+    @IsNotEmpty()
+    @IsNumber()
+    encargadoId: number;
+
+    @IsNotEmpty()
+    @IsNumber()
+    enfermedadId: number;
+
+    @IsNotEmpty()
+    @IsNumber()
+    turnoId: number;
+
+    @IsNotEmpty()
+    @IsNumber()
+    administradorId: number;
+
+    @IsNotEmpty()
+    @IsNumber()
+    padrinoId: number;
+}
