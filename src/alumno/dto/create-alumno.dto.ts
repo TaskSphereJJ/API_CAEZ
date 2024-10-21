58be85dbf7a8fd18ee973f8d12a5b4b590f91686
Rodrigo Reyes <rodrigoreyes2902@gmail.com>
2024-10-21 11:04:00 -0600

    :tada::sparkles:

--- src/alumno/dto/create-alumno.dto.ts
+++ src/alumno/dto/create-alumno.dto.ts
@@ -1,1 +1,51 @@
-export class CreateAlumnoDto {}
+import { IsBoolean, IsDate, IsNotEmpty, IsNumber, IsString } from "class-validator";
+
+export class CreateAlumnoDto {
+    @IsNotEmpty()
+    @IsString()
+    name: string;
+
+    @IsNotEmpty()
+    @IsString()
+    lastName: string;
+
+    @IsNotEmpty()
+    @IsDate()
+    registrationDate: Date;
+
+    @IsNotEmpty()
+    @IsNumber()
+    documentNumber: number; 
+
+    @IsNotEmpty()
+    @IsBoolean()
+    esBecado: boolean;
+
+    @IsNotEmpty()
+    @IsNumber()
+    sexoId: number;
+
+    @IsNotEmpty()
+    @IsNumber()
+    roleId: number;
+
+    @IsNotEmpty()
+    @IsNumber()
+    encargadoId: number;
+
+    @IsNotEmpty()
+    @IsNumber()
+    enfermedadId: number;
+
+    @IsNotEmpty()
+    @IsNumber()
+    turnoId: number;
+
+    @IsNotEmpty()
+    @IsNumber()
+    administradorId: number;
+
+    @IsNotEmpty()
+    @IsNumber()
+    padrinoId: number;
+}
