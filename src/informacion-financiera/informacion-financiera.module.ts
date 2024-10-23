cd5675bbfef48348780a23423a1f70e48c8e056e
Rodrigo Reyes <rodrigoreyes2902@gmail.com>
2024-10-22 22:18:07 -0600

    modulo informacion-financiera :tada::sparkles:

--- src/informacion-financiera/informacion-financiera.module.ts
+++ src/informacion-financiera/informacion-financiera.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { InformacionFinancieraService } from './informacion-financiera.service';
 import { InformacionFinancieraController } from './informacion-financiera.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { InformacionFinanciera } from './entities/informacion-financiera.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([InformacionFinanciera])],
   controllers: [InformacionFinancieraController],
   providers: [InformacionFinancieraService],
 })
