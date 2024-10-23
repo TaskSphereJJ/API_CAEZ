dafad3f0536426c83adfc0c2a75777a0dac1f7db
Rodrigo Reyes <rodrigoreyes2902@gmail.com>
2024-10-22 22:12:25 -0600

    modulo factura :tada::sparkles:

--- src/factura/factura.module.ts
+++ src/factura/factura.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { FacturaService } from './factura.service';
 import { FacturaController } from './factura.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Factura } from './entities/factura.entity';
+import { Pago } from 'src/pago/entities/pago.entity';
+import { Alumno } from 'src/alumno/entities/alumno.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Factura, Pago, Alumno])],
   controllers: [FacturaController],
   providers: [FacturaService],
 })
