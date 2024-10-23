9ad2d2c8f9e9ff67e54952772997909aa496e2af
Rodrigo Reyes <rodrigoreyes2902@gmail.com>
2024-10-22 22:14:23 -0600

    create informacion-financiera :tada::sparkles:

--- src/informacion-financiera/dto/create-informacion-financiera.dto.ts
+++ src/informacion-financiera/dto/create-informacion-financiera.dto.ts
@@ -1,1 +1,19 @@
-export class CreateInformacionFinancieraDto {}
+import { IsDecimal, IsNotEmpty, IsNumber } from "class-validator";
+
+export class CreateInformacionFinancieraDto {
+    @IsNotEmpty()
+    @IsNumber()
+    fondoActual: number;
+
+    @IsNotEmpty()
+    @IsDecimal()
+    deudaTotal: number;
+
+    @IsNotEmpty()
+    @IsDecimal()
+    salgoPorCompletar: number;
+
+    @IsNotEmpty()
+    @IsNumber()
+    numStudent: number;
+}
