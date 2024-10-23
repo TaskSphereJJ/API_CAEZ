31625129a2d731e3ecf88f507a3ce2c2ed326e3b
Rodrigo Reyes <rodrigoreyes2902@gmail.com>
2024-10-22 22:13:50 -0600

    update grado :tada::sparkles:

--- src/grados/dto/update-grado.dto.ts
+++ src/grados/dto/update-grado.dto.ts
@@ -1,4 +1,2 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateGradoDto } from './create-grado.dto';
 
-export class UpdateGradoDto extends PartialType(CreateGradoDto) {}
+export class UpdateGradoDto {}
