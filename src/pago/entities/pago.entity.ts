e08ed47880b70bc322b4ef392ec12e0ad68ad23b
Rodrigo Reyes <rodrigoreyes2902@gmail.com>
2024-10-23 22:59:00 -0600

    entidad pago :tada::sparkles:

--- src/pago/entities/pago.entity.ts
+++ src/pago/entities/pago.entity.ts
@@ -1,1 +1,42 @@
-export class Pago {}
+import { Alumno } from "src/alumno/entities/alumno.entity";
+import { TipoPago } from "src/tipo-pago/entities/tipo-pago.entity";
+import { User } from "src/users/entities/user.entity";
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('pago')
+export class Pago {
+    @PrimaryGeneratedColumn()
+    Id: number;
+
+    @ManyToOne(() => Alumno)
+    alumnoId: Alumno;
+
+    @Column()
+    multa: number;
+
+    @ManyToOne(() => TipoPago)
+    tipoPagoId: TipoPago;
+
+    @Column()
+    discount: number;
+
+    @Column()
+    price: number;
+
+    @Column()
+    totalPagado: number;
+
+    @Column({type: 'date'})
+    registrationDate: Date;
+
+    @ManyToOne(() => User)
+    adminId: User;
+
+    @Column()
+    description: string;
+
+    @Column({default: true})
+    isActive: boolean;
+
+
+}
