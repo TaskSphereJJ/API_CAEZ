67552e5f7dd896e68cca429f7956b43a667c0617
Rodrigo Reyes <rodrigoreyes2902@gmail.com>
2024-10-23 22:58:00 -0600

    modulo pago :tada::sparkles:

--- src/pago/pago.module.ts
+++ src/pago/pago.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { PagoService } from './pago.service';
 import { PagoController } from './pago.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Pago } from './entities/pago.entity';
+import { Alumno } from 'src/alumno/entities/alumno.entity';
+import { TipoPago } from 'src/tipo-pago/entities/tipo-pago.entity';
+import { User } from 'src/users/entities/user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Pago, Alumno, TipoPago, User])],
   controllers: [PagoController],
   providers: [PagoService],
 })
