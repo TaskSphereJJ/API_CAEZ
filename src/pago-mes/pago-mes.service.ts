f90e6ba57a20604fed04705967c5b43b02fe848f
Rodrigo Reyes <rodrigoreyes2902@gmail.com>
2024-10-23 22:59:00 -0600

    servicio pago mes :tada::sparkles:

--- src/pago-mes/pago-mes.service.ts
+++ src/pago-mes/pago-mes.service.ts
@@ -1,14 +1,74 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePagoMeDto } from './dto/create-pago-me.dto';
 import { UpdatePagoMeDto } from './dto/update-pago-me.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { PagoMe } from './entities/pago-me.entity';
+import { Repository } from 'typeorm';
+import { Pago } from 'src/pago/entities/pago.entity';
+import { Mes } from 'src/mes/entities/me.entity';
 
 @Injectable()
 export class PagoMesService {
-
+ constructor(
+  @InjectRepository(PagoMe)private readonly pagoMesRepository: Repository<PagoMe>,
+  @InjectRepository(Pago)private readonly pagosRepository: Repository<Pago>,
+  @InjectRepository(Mes)private readonly mesRepository: Repository<Mes>
+ ){}
 
   // Crear pago mes
-  create(createPagoMeDto: CreatePagoMeDto) {
-    return 'This action adds a new pagoMe';
+  async create(createPagoMeDto: CreatePagoMeDto) {
+    try {
+      // Verifico que exista pago por medio del id
+      // porque no funciona el id??
+      const pago = await this.pagosRepository.findOne({
+        where: {Id: createPagoMeDto.pagoId}
+      });
+
+      // Verifico que la respuesta no sea nula
+      if(!pago){
+        return {
+          ok: false,
+          message: 'Pago no encontrado',
+          status: 404,
+        };
+      }
+
+      // Verifico que existe mes por medio del id
+      const mes = await this.mesRepository.findOne({
+        where: {id: createPagoMeDto.mesId}
+      });
+
+      // Evaluo que la respuesta no sea nula
+      if(!mes){
+        return {
+          ok: false,
+          message: 'Pago Mes no encontrado',
+          status: 404
+        }
+      }
+
+      // Instancia de la entidad Pago - Mes
+      const pagoMes = new PagoMe();
+      pagoMes.pagoId = pago;
+      pagoMes.mesId = mes;
+
+      // Guardo los datos en la base
+      await this.pagosRepository.save(pagoMes);
+       
+      // Mensaje de exito al crear un pago mes
+      return {
+        ok: true,
+        message: 'No se enconro el Pago Mes',
+        status: 201,
+      };
+
+    } catch (error) {
+      return {
+        ok: false,
+        message: error.message,
+        status:500,
+      };
+    }
   }
 
   findAll() {
