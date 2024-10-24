ea1c14c1f7e5aa5523d86cac33b09b3979de7e22
Rodrigo Reyes <rodrigoreyes2902@gmail.com>
2024-10-23 22:59:00 -0600

    create padrino :tada::sparkles:

--- src/padrino/dto/create-padrino.dto.ts
+++ src/padrino/dto/create-padrino.dto.ts
@@ -1,1 +1,38 @@
-export class CreatePadrinoDto {}
+import { IsDate, IsNotEmpty, IsNumber, IsString } from "class-validator";
+
+export class CreatePadrinoDto {
+    @IsNotEmpty()
+    @IsString()
+    name: string;
+
+    @IsNotEmpty()
+    @IsString()
+    lastName: string;
+
+    @IsNotEmpty()
+    phone: number;
+
+    @IsNotEmpty()
+    @IsString()
+    email: string;
+
+    @IsNotEmpty()
+    @IsDate()
+    registrationDate: Date;
+
+    @IsNotEmpty()
+    @IsNumber()
+    roleId: number;
+
+    @IsNotEmpty()
+    @IsNumber()
+    sexoId: number;
+
+    @IsNotEmpty()
+    @IsNumber()
+    direccionId: number;
+
+    @IsNotEmpty()
+    @IsNumber()
+    administradorId: number;
+}
