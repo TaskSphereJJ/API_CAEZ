865979236ef76d4f5f80a31920da14962f3bb464
Rodrigo Reyes <rodrigoreyes2902@gmail.com>
2024-10-23 22:59:00 -0600

    create pago :tada::sparkles:

--- src/pago/dto/create-pago.dto.ts
+++ src/pago/dto/create-pago.dto.ts
@@ -1,1 +1,39 @@
-export class CreatePagoDto {}
+import { IsDate, IsNotEmpty, IsNumber, IsString } from "class-validator";
+
+export class CreatePagoDto {
+    @IsNotEmpty()
+    @IsNumber()
+    alumnoId: number;
+
+    @IsNotEmpty()
+    @IsNumber()
+    multa: number;
+
+    @IsNotEmpty()
+    @IsNumber()
+    tipoPagoId: number;
+
+    @IsNotEmpty()
+    @IsNumber()
+    discount: number;
+
+    @IsNotEmpty()
+    @IsNumber()
+    price: number;
+
+    @IsNotEmpty()
+    @IsNumber()
+    totalPagado: number;
+
+    @IsNotEmpty()
+    @IsDate()
+    registrationDate: Date;
+
+    @IsNotEmpty()
+    @IsNumber()
+    adminId: number;
+
+    @IsNotEmpty()
+    @IsString()
+    description: string;
+}
