d325c558320cec1afbc0a091f808190380ca8751
Rodrigo Reyes <rodrigoreyes2902@gmail.com>
2024-10-25 22:52:00 -0600

    create direccion :tada::sparkles:

--- src/direccion/dto/create-direccion.dto.ts
+++ src/direccion/dto/create-direccion.dto.ts
@@ -1,1 +1,7 @@
-export class CreateDireccionDto {}
+import { IsNotEmpty, IsString } from "class-validator";
+
+export class CreateDireccionDto {
+    @IsNotEmpty()
+    @IsString()
+    name: string;
+}
