49e8f930f359e531e5cd7538096b8c2486c39ce0
Rodrigo Reyes <rodrigoreyes2902@gmail.com>
2024-11-06 21:07:21 -0600

    create tipo pago :tada::sparkles:

--- src/tipo-pago/dto/create-tipo-pago.dto.ts
+++ src/tipo-pago/dto/create-tipo-pago.dto.ts
@@ -1,1 +1,7 @@
-export class CreateTipoPagoDto {}
+import { IsNotEmpty, IsString } from "class-validator";
+
+export class CreateTipoPagoDto {
+    @IsNotEmpty()
+    @IsString()
+    name: string;
+}
