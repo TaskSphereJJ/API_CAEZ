8aa2bf414e6b025417dccd1859b78b6f62290f22
Rodrigo Reyes <rodrigoreyes2902@gmail.com>
2024-11-07 19:55:06 -0600

    dto update rol :tada::sparkles:

--- src/roles/dto/update-role.dto.ts
+++ src/roles/dto/update-role.dto.ts
@@ -1,4 +1,7 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateRoleDto } from './create-role.dto';
+import { IsNotEmpty, IsString } from "class-validator";
 
-export class UpdateRoleDto extends PartialType(CreateRoleDto) {}
+export class UpdateRoleDto {
+    @IsNotEmpty()
+    @IsString()
+    name: string;
+}
