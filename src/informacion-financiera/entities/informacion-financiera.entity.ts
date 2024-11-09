5d5a1f64f3e7929cf5feeb0abfb0083924885cfa
Rodrigo Reyes <rodrigoreyes2902@gmail.com>
2024-11-08 20:24:26 -0600

    entidad información financiera :tada::sparkles:

--- src/informacion-financiera/entities/informacion-financiera.entity.ts
+++ src/informacion-financiera/entities/informacion-financiera.entity.ts
@@ -3,7 +3,7 @@ import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 @Entity('informacionFinanciera')
 export class InformacionFinanciera {
     @PrimaryGeneratedColumn()
-    Id: number;
+    id: number;
 
     @Column()
     fondoActual: number;
